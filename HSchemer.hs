ffda2d18436b6eaeb2e525f18b855612b23a61cd
Philip Hayes <philiphayes9@gmail.com>
2013-11-11 23:56:53 -0800

    Fill out parser some more

--- HSchemer.hs
+++ HSchemer.hs
@@ -16,33 +16,8 @@ data LispVal = Atom String
 instance Show LispVal where
         show (Number n) = show n
         show (String s) = show s
-        show (Bool _) | True = "#t"
-                      | False = "#f"
-
-parseString :: Parser LispVal
-parseString = do char '"'
-                 x <- many $ noneOf "\""
-                 char '"'
-                 return $ String x
-
-parseAtom :: Parser LispVal
-parseAtom = do first <- letter <|> symbol
-               rest <- many $ letter <|> digit <|> symbol 
-               let atom = first : rest
-               return $ case atom of
-                             "#t" -> Bool True
-                             "#f" -> Bool False
-                             otherwise -> Atom atom
-
-parseNumber :: Parser LispVal
-parseNumber = do ds <- many1 digit
-                 return $ liftM (Number . read) ds
-{-parseNumber = liftM (Number . read) $ many1 digit-}
-
-parseExpr :: Parser LispVal
-parseExpr = parseAtom
-        <|> parseString
-        <|> parseNumber
+        show (Bool True) = "#t"
+        show (Bool False) = "#f"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -50,11 +25,39 @@ spaces = skipMany1 space
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
-readExpr :: String -> String
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol <|> char '#'
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first : rest
+               return $ case atom of
+                    "#t" -> Bool True
+                    "#f" -> Bool False
+                    _ -> Atom atom
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many chars
+                 char '"'
+                 return $ String x
+    where chars = escapedChar <|> noneOf "\""
+          escapedChar = char "\\" >> choice 
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-        Left err -> "No match: " ++ show err
-        Right val -> "Found value: " ++ show val
+        Left err -> String $ "Parse error: " ++ show err
+        Right val -> val
+
+printExpr :: String -> IO ()
+printExpr input = print $ readExpr input
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn $ readExpr $ head args          
+          print $ readExpr $ head args
